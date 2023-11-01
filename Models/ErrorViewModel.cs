a871a08939298c7210d7fb4e24b11f64985ea09f
PieDePopper <2200279ogg@zuyd.nl>
2023-11-01 21:15:17 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BioWiseV2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
