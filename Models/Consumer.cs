8850db3d40696b1bb03c0a55c06f0817869003eb
MerlijnW3 <merlijnwaaijer03@gmail.com>
2023-11-06 11:40:13 +0100

    Migration added consumer name

--- Models/Consumer.cs
+++ Models/Consumer.cs
@@ -3,6 +3,7 @@
     public class Consumer
     {
         public int Id { get; set; }
+        public string Name { get; set; }
 
         // one to many (TransportUsage)
         public int? TransportUsageId { get; set; }
