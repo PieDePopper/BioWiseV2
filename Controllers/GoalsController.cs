127fe18e4f34b46257e38b00c78aca918f8ba188
MerlijnW3 <129836037+MerlijnW3@users.noreply.github.com>
2023-11-07 14:04:19 +0100

    test

--- Controllers/GoalsController.cs
+++ Controllers/GoalsController.cs
@@ -29,6 +29,7 @@ namespace BioWiseV2.Controllers
             return View(await applicationDbContext.ToListAsync());
         }
 
+
         // GET: Goals/Details/5
         public async Task<IActionResult> Details(int? id)
         {
