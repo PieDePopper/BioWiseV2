035b3fc53ee3eed09b35b303e20cad7b250741fd
MerlijnW3 <merlijnwaaijer03@gmail.com>
2023-11-09 09:43:43 +0100

    migration weekly report update

---
+++ Data/Migrations/20231109084322_updatedweeklyreport.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BioWiseV2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedweeklyreport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Transport_emission",
+                table: "Weekly_report",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Total",
+                table: "Weekly_report",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Difference",
+                table: "Weekly_report",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Transport_emission",
+                table: "Weekly_report",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Total",
+                table: "Weekly_report",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Difference",
+                table: "Weekly_report",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
--- Models/Weekly_report.cs
+++ Models/Weekly_report.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public int Weeknr { get; set; }
-        public int Transport_emission { get; set; }
-        public int Total { get; set; }
-        public int Difference { get; set; }
+        public int? Transport_emission { get; set; }
+        public int? Total { get; set; }
+        public int? Difference { get; set; }
 
         //one to many (TransportUsage)
         public int? TransportusageId { get; set; }
